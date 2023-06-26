2da597e5cc0e4b03bdb817e6e61f57f7e8b735a4
Dario Lombardo <lomato@gmail.com>
2023-06-26 15:10:58 +0200

    test: add appendix c tests.

---
+++ test/test_appendix.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class AppendixTest < Stix2::Test
+  def test_appendix
+    4.times do |i|
+      stix_messages("appendix_c_#{i + 1}").each do |message|
+        assert Stix2.parse(message)
+      end
+    end
+  end
+end
