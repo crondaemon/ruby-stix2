7011372a2c53a2248900d7dc2300070e4252ce48
Dario Lombardo <lomato@gmail.com>
2024-04-10 08:31:09 +0200

    bundle: remove useless id.

--- lib/stix2/bundle.rb
+++ lib/stix2/bundle.rb
@@ -1,7 +1,6 @@
 module Stix2
   class Bundle < Stix2::Common
     property :type, required: true, coerce: String
-    property :id, coerce: String
     property :objects, coerce: ->(values) { values.map { Stix2.parse(_1) } }
   end
 end
