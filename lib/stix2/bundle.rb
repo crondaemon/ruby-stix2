6d66bc939967bc2df4a3b24deb6b4536ae0692b2
Dario Lombardo <lomato@gmail.com>
2024-05-06 15:18:27 +0200

    bundle: fix some issues in coercion.

--- lib/stix2/bundle.rb
+++ lib/stix2/bundle.rb
@@ -1,6 +1,6 @@
 module Stix2
   class Bundle < Stix2::Common
     property :type, required: true, coerce: String
-    property :objects, coerce: ->(values) { values.map { Stix2.parse(_1) } }
+    property :objects, coerce: ->(array) { array.all? { |element| element.is_a?(::Stix2::Common) || raise("Invalid Object") } && array }
   end
 end
