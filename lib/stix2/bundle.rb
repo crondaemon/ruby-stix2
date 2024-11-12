c178792e95e3cd2d3a93f370617e6eb19c2cf752
Dario Lombardo <lomato@gmail.com>
2024-11-12 16:53:13 +0100

    bundle: allow objects as stix as well as hashes.

--- lib/stix2/bundle.rb
+++ lib/stix2/bundle.rb
@@ -1,6 +1,10 @@
 module Stix2
   class Bundle < Stix2::Common
     property :type, required: true, coerce: String
-    property :objects, coerce: ->(array) { array.all? { |element| element.is_a?(::Stix2::Common) || raise("Invalid Object") } && array }
+    property :objects, coerce: ->(array) do
+      array.all? do |element|
+        element.is_a?(::Stix2::Common) || Stix2.parse(element).is_a?(::Stix2::Common) || raise("Invalid Object")
+      end && array
+    end
   end
 end
