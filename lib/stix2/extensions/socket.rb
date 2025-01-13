970f75a3c7b9f6e41f79cbfd999cf6af8d9f0c60
Dario Lombardo <lomato@gmail.com>
2025-01-13 16:18:04 +0100

    socket: fix options coercion.

--- lib/stix2/extensions/socket.rb
+++ lib/stix2/extensions/socket.rb
@@ -4,7 +4,7 @@ module Stix2
       property :address_family, required: true, values: NETWORK_SOCKET_ADDRESS_FAMILY_ENUM
       property :is_blocking, coerce: Stix2.bool
       property :is_listening, coerce: Stix2.bool
-      property :options, coerce: ->(hsh) { hsh.keys.all? { |k| k.is_a?(Integer) } && hsh }
+      property :options, coerce: {String => Integer}
       property :socket_type, values: NETWORK_SOCKET_TYPE_ENUM
       property :socket_descriptor, coerce: Integer
       property :socket_handle, coerce: Integer
